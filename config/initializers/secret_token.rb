90b75061075a5b31e4ec89468e6d2900cbecb494
Wei Lu <iamweilu@gmail.com>
2011-04-15 05:47:17 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardWall::Application.config.secret_token = '3e77849352eb6a8c69a884b9d454ea55cc0f03f4bbfa5f8ed46e785f40d36fd1b5c3abb50ec01bace3f05b0e04cbb48b64a7734c9e515cc1b1f0217c373c4198'
